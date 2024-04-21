a76d4e6b1c93bf508879dac2b74a0188255b8c9f
Anton Zudin <zuas21a@econ.msu.ru>
2024-04-21 22:41:49 +0300

    Fixed a bug in weights_scatteplot function

--- R/weights_scatterplot.R
+++ R/weights_scatterplot.R
@@ -1,19 +1,19 @@
 #' Draw a comparative scatterplot for original weights and robust weights.
 #' 
 #' @param robust_estimate The object to use for building the plot.
-#' @param save_pdf  If True, the scatterplot is saved.
 #' @param folder The folder where the plots are saved. NULL stands for the working directory.
+#' @param save_pdf  If True, the scatterplot is saved.
 #' @param height The height of the plot.
 #' @param width The width of the plot.
 #' @export
 #'
 
 weights_scatterplot <- function(robust_estimate, folder = NULL, 
-                                save_pdf = FALSE, width = 9, height = 9) {
+                                save_pdf = FALSE, height = 9, width = 9) {
   
-  basic_res <- robust_estimates$result
-  index_sub <- robust_estimates$index_sub
-  state_names <- robust_estimates$state_names
+  basic_res <- robust_estimate$result
+  index_sub <- robust_estimate$index_sub
+  state_names <- robust_estimate$state_names
   
   or_weights <- basic_res[[1]][,1]/sd(basic_res[[1]][,1])
   rob_weights <- basic_res[[1]][,2]/sd(basic_res[[1]][,2])
@@ -36,5 +36,4 @@ weights_scatterplot <- function(robust_estimate, folder = NULL,
     text(or_weights,rob_weights, labels=state_names[index_sub], cex=0.7, font=1)
     abline(a = 0, b = 1, lty = 2, lwd = 0.5, col = 'grey')
   }
-
 }  
