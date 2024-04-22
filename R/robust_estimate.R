86be127e9842344f16dddb704a465a35b5df40af
Anton Zudin <zuas21a@econ.msu.ru>
2024-04-22 08:59:40 +0300

    Fixed a bug in robust_estimate (index_sub didn't initialize  when it was NULL)

--- R/robust_estimate.R
+++ R/robust_estimate.R
@@ -2,7 +2,7 @@
 #' @param Y_mat_or an nxT matrix of outcome.
 #' @param W_mat_or an nxT matrix of endogenous treatment.
 #' @param Z an nx1 vector of an aggregate instrument.
-#' @param index_sub an nx1 vector with binary coordinates. If one of the coordinates is TRUE, we include this state in the estimation.
+#' @param index_sub an nx1 vector with binary coordinates. If one of the coordinates is TRUE, we include this state in the estimation.NULL stands for selecting all the units.
 #' @param T_0 the size of the learning period.
 #' @param unit_names an nx1 vector with unit names. It is used only in plot_1 function. 
 #' @param time_column If true, the units are in the rows and the time periods are in the columns in Y_mat_or and W_mat_or.
@@ -14,14 +14,17 @@
 
 
 
-robust_estimate <- function(Y_mat_or, W_mat_or, Z, unit_covariates = NULL, time_covariates = NULL, index_sub = NULL, T_0 = NULL, 
-                            unit_names = NULL, time_column = TRUE, seed = 1234){
+robust_estimate <- function(Y_mat_or, W_mat_or, Z, 
+                            unit_covariates = NULL, time_covariates = NULL, 
+                            index_sub = NULL, T_0 = NULL,
+                            unit_names = NULL, time_column = TRUE, 
+                            seed = 1234){
   
   if (time_column == TRUE) {
     Y_mat_or <- as.matrix(Y_mat_or)
     W_mat_or <- as.matrix(W_mat_or)
     Z <- as.vector(Z)
-  } else{
+  } else {
     Y_mat_or <- t(as.matrix(Y_mat_or))
     W_mat_or <- t(as.matrix(W_mat_or))
     Z <- as.vector(Z)
@@ -31,6 +34,8 @@ robust_estimate <- function(Y_mat_or, W_mat_or, Z, unit_covariates = NULL, time_
   if (!is.null(index_sub)){
     Y_mat_or <- Y_mat_or[index_sub,]
     W_mat_or <- W_mat_or[index_sub,]
+  } else {
+    index_sub <- rep(TRUE, length.out = nrow(Y_mat_or))
   }
   
   n <- dim(Y_mat_or)[1]
