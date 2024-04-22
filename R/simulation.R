0ffa404b688398b8e83e1f987d5410b761e8976b
Anton Zudin <zuas21a@econ.msu.ru>
2024-04-22 13:39:59 +0300

    Added an essential comma

--- R/simulation.R
+++ R/simulation.R
@@ -31,7 +31,7 @@ simulation <- function(Y_mat_or, W_mat_or, Z,
                        share_t = 1/3, share_rank = 1/3, 
                        rho_agg = 0.5, rho_theta_w = 0.2, rho_theta_y = 0.3, 
                        B = 1000, S = 300, 
-                       test = FALSE, return_table = FALSE
+                       test = FALSE, return_table = FALSE,
                        K = 300, deg = 4, 
                        height = 9*0.75, width = 16*0.75, 
                        plot_folder = NULL, save_sim = FALSE, save_pdf = FALSE, 
@@ -211,7 +211,7 @@ simulation <- function(Y_mat_or, W_mat_or, Z,
     abline(v = 0,lwd = 1,lty =2)
     legend('topright',lty = c(1,2),legend = c('Robust','TSLS'))
     
-    plot(dens_our_des_4[,c(1,3)],lwd = 2, xlim = c(-1.5, 1.5), type = 'l',lty = 1,xlab = 'estimate',
+    plot(dens_our_des_4[,c(1,3)],lwd = 2, xlim = c(-1.5, 1.5), type = 'l',lty = 1, xlab = 'estimate',
          ylab = 'Density',main = 'Unobserved shocks')
     lines(dens_tsls_des_4 [,c(1,3)],lwd = 2,lty = 2)
     abline(v = 0,lwd = 1,lty =2)
@@ -221,16 +221,16 @@ simulation <- function(Y_mat_or, W_mat_or, Z,
   } else {
     par(mfrow=c(1,2)) 
     
-    plot(dens_our_des_2[,c(1,3)],lwd = 2, xlim = c(-1.5,1.5), type = 'l',lty = 1,xlab = 'estimate',
+    plot(dens_our_des_2[, c(1,3)] ,lwd = 2, xlim = c(-1.5,1.5), type = 'l', lty = 1, xlab = 'estimate',
          ylab = 'Density',main = 'No unobserved shocks')
-    lines(dens_tsls_des_2 [,c(1,3)],lwd = 2,lty = 2)
-    abline(v = 0,lwd = 1,lty =2)
-    legend('topright',lty = c(1,2),legend = c('Robust','TSLS'))
+    lines(dens_tsls_des_2 [, c(1,3)], lwd = 2, lty = 2)
+    abline(v = 0, lwd = 1, lty =2)
+    legend('topright', lty = c(1,2), legend = c('Robust','TSLS'))
     
-    plot(dens_our_des_4[,c(1,3)],lwd = 2, xlim = c(-1.5, 1.5), type = 'l',lty = 1,xlab = 'estimate',
-         ylab = 'Density',main = 'Unobserved shocks')
-    lines(dens_tsls_des_4 [,c(1,3)],lwd = 2,lty = 2)
-    abline(v = 0,lwd = 1,lty =2)
+    plot(dens_our_des_4[,c(1,3)], lwd = 2, xlim = c(-1.5, 1.5), type = 'l', lty = 1, xlab = 'estimate',
+         ylab = 'Density', main = 'Unobserved shocks')
+    lines(dens_tsls_des_4 [,c(1,3)], lwd = 2, lty = 2)
+    abline(v = 0, lwd = 1, lty =2)
   }
   
   
